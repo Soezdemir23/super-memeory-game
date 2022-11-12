3b9cfa18116c4c41a8acdaba90981a97af824c5e
Soezdemir23 <soezdemir3423@gmail.com>
2022-11-12 09:02:33 +0100

    Accidentally removed and deleted very important stylization, better commit what I have now before it goes away the same way

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,18 @@
+interface cardProps {
+  urls: null | string[];
+}
 
-export default function Main() {
-    return(
-        <main className="flex-1">
-            shceudle
-        </main>
-    )
+export default function Main({ urls }: cardProps) {
+  let count = 0;
+  // need to think of something when I can display a "Loading screen component" until all images are loaded
+
+  return (
+    <main>
+      {urls?.map((url) => (
+        <div>
+          <img src={url} alt=""></img>
+        </div>
+      )) }
+    </main>
+  )
 }
