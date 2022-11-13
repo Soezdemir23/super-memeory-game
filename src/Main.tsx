32e37c6469818ba0588ba3b306eeacf62ec02ed7
Soezdemir23 <soezdemir3423@gmail.com>
2022-11-13 17:49:07 +0100

    Work on UI, add comments for todo later, add function to count loaded images

--- src/Main.tsx
+++ src/Main.tsx
@@ -5,14 +5,27 @@ interface cardProps {
 export default function Main({ urls }: cardProps) {
   let count = 0;
   // need to think of something when I can display a "Loading screen component" until all images are loaded
+  const allLoaded = () => {
+    count++;
+    if (count === urls?.length) {
+      count = 0;
+      return true;
+    }
+    return false;
+  };
 
+  /**So I want the card to not only enlarge, but I also want the image to transform a 
+   * bit based on the mouse position when it is hovered on it.
+   * Then I also want it to shine, when I press on it and keep the shiny background, animation
+   * Then ... I don't know anymore
+   */
   return (
-    <main>
+    <main className="grid gap-x-1 gap-y-5 pt-8 hover: justify-items-center grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
       {urls?.map((url) => (
-        <div>
-          <img src={url} alt=""></img>
+        <div className="w-32 sm:w-40 xl:w-52 hover:y">
+          <img className="rounded-xl" onLoad={allLoaded} src={url} alt=""></img>
         </div>
-      )) }
+      ))}
     </main>
-  )
+  );
 }
