d725bb2f861df7b33b21bbde88c1024f5461b857
Soezdemir23 <soezdemir3423@gmail.com>
2022-11-14 13:46:01 +0100

    testing new keyframes and animation implemented in tailwind.config.js

--- src/Main.tsx
+++ src/Main.tsx
@@ -14,7 +14,13 @@ export default function Main({ urls }: cardProps) {
     return false;
   };
 
-  /**So I want the card to not only enlarge, but I also want the image to transform a 
+  //call a function that returns spin on the card.
+  const spinClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    spin();
+  };
+
+  function spin() {}
+  /**So I want the card to not only enlarge, but I also want the image to transform a
    * bit based on the mouse position when it is hovered on it.
    * Then I also want it to shine, when I press on it and keep the shiny background, animation
    * Then ... I don't know anymore
@@ -22,8 +28,20 @@ export default function Main({ urls }: cardProps) {
   return (
     <main className="grid gap-x-1 gap-y-5 pt-8 hover: justify-items-center grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
       {urls?.map((url) => (
-        <div className="w-32 sm:w-40 xl:w-52 hover:y">
-          <img className="rounded-xl" onLoad={allLoaded} src={url} alt=""></img>
+        <div className="hover:scale-150 w-32 sm:w-40 xl:w-52">
+          <div
+            className={
+              " hover:animate-spin-horizontal"
+            }
+            
+          >
+            <img
+              className="rounded-xl"
+              onLoad={allLoaded}
+              src={url}
+              alt=""
+            ></img>
+          </div>
         </div>
       ))}
     </main>
