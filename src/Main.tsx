17bab96cce8f70cabe9dfe3c7709b22c055bf798
Soezdemir23 <soezdemir3423@gmail.com>
2022-11-16 14:42:56 +0100

    move interface into interfaces.ts and expanded on the pepeobject being rendered

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,8 +1,6 @@
-interface cardProps {
-  urls: null | string[];
-}
+import { cardProps } from "./interfaces";
 
-export default function Main({ urls }: cardProps) {
+export default function Main({ urls, onClick }: cardProps) {
   let count = 0;
   // need to think of something when I can display a "Loading screen component" until all images are loaded
   const allLoaded = () => {
@@ -14,12 +12,8 @@ export default function Main({ urls }: cardProps) {
     return false;
   };
 
-  //call a function that returns spin on the card.
-  const spinClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    spin();
-  };
+  
 
-  function spin() {}
   /**So I want the card to not only enlarge, but I also want the image to transform a
    * bit based on the mouse position when it is hovered on it.
    * Then I also want it to shine, when I press on it and keep the shiny background, animation
@@ -28,17 +22,17 @@ export default function Main({ urls }: cardProps) {
   return (
     <main className="grid gap-x-1 gap-y-5 pt-8 hover: justify-items-center grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
       {urls?.map((url) => (
-        <div className="hover:scale-150 w-32 sm:w-40 xl:w-52">
-          <div
-            className={
-              " hover:animate-spin-horizontal"
-            }
-            
-          >
+        <div
+          key={url.id}
+          data-key={url.id}
+          className="hover:scale-110 w-32 sm:w-40 xl:w-52"
+          onClick={onClick}
+        >
+          <div className={url.pressed === true ? "animate-spin-horizontal" : ""}>
             <img
               className="rounded-xl"
               onLoad={allLoaded}
-              src={url}
+              src={url.url}
               alt=""
             ></img>
           </div>
