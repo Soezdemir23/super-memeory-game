85f50e21ce8d0c7951472244bcd3d03cbcb04351
Soezdemir23 <soezdemir3423@gmail.com>
2022-11-16 14:41:25 +0100

    Add spinConfirmClick function to implement boolean based spin animation

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Main from './Main';
 import Footer from './Footer';
 import feed from  './feed.json'
 import {useState, useEffect} from 'react';
-
+import {pepeObject} from './interfaces'
 function App() {
-  const [usedFeeds,setUsedFeeds] = useState<null | string[]>(null)
+  const [usedFeeds,setUsedFeeds] = useState<null | undefined | pepeObject[]>(null)
 
   function pickPepeURLs(numOfCards: number) {
-    const urlArray: string[] = []
-    while (urlArray.length < numOfCards) {
+    const pepeList: pepeObject[] = []
+    while(pepeList.length < numOfCards) {
+      
       // create a random number to pick a number
       let randomNumber = Math.ceil(Math.random() * Object.keys(feed).length)
       // then return a key from the feed.json's Object key array
@@ -18,27 +19,40 @@ function App() {
       // we are traversing through the loop and use key, value of loop
       // this allows me to skip the object typing I am not sure yet how to do it
       for (const [key, value] of Object.entries(feed)) {
-        // if the key is found, return back to the while loop if it exists in the array
-        if(key === feedKey) {
-          if (urlArray.includes(value.img_url) === true) {
+        // we traverse the feed, see if the random word exists
+        if (key === feedKey )
+        // check if the random word 
+          if( pepeList.filter((pepe) => pepe.id === key).length > 0 ) {
             break;
+          } else {
+            // push to the pepeList that we will later set
+            pepeList.push({id: key, pressed: false, url: value.img_url})
           }
-          // else add the address into the array
-          urlArray.push(value.img_url);
         }
       }
+      return pepeList;
     }
-    return urlArray;
-  }
+  
   // A kind user told me, that i am setting state on every render if I don't use it in this manner
   // this is an initial render
   useEffect(()=>{
     setUsedFeeds(pickPepeURLs(40))
   }, [])
+
+  function spinConfirmClick(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
+    console.log(e.currentTarget.dataset.key)
+    const changedPepes = usedFeeds?.map((pepe) => {
+      if (pepe.id === e.currentTarget.dataset.key) {
+        pepe.pressed = !pepe.pressed;
+        return pepe;
+      }return pepe;
+    })
+    setUsedFeeds(changedPepes)
+  }
   return (
     <div className='flex flex-col min-h-screen bg-green-600 '>
       <Header />
-      <Main urls={usedFeeds}/>
+      <Main urls={usedFeeds} onClick={spinConfirmClick}/>
       
       <Footer />
     </div>
